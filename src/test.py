6c871be2e1fcce29e97bc0ea8f5170f37f56b1eb
masashi2ueda <masashi620@gmail.com>
2024-08-15 21:23:55 +0900

    Dynp

--- src/test.py
+++ src/test.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime
+import ruptures as rpt
 
 import create_sim_data
 
@@ -16,6 +17,12 @@ start_dates = [
     datetime.datetime(2020, 12, 1),
     datetime.datetime(2021, 4, 1),
     datetime.datetime(2021, 12, 1)
+]
+season_changes = [
+    datetime.datetime(2020, 7, 1),
+    datetime.datetime(2021, 1, 1),
+    datetime.datetime(2021, 7, 1),
+    datetime.datetime(2022, 1, 1),
 ]
 
 date_df, date_dfs = create_sim_data.create_x_dates(
@@ -32,3 +39,33 @@ for i in range(4):
     plt.plot(date_dfs[i]["day"], date_dfs[i]["x"], label=str(i))
     plt.plot(date_df["day"], date_df["x"], label="dst")
     plt.legend()
+
+# %%
+xs = date_df["x"].values
+
+# %%
+
+
+# コスト関数の設定
+model = "l2"
+# アルゴの設定と学習
+algo = rpt.Dynp(model=model).fit(xs)
+# 変化点の検出
+my_bkps = algo.predict(n_bkps=3)
+
+
+# %%
+plt.figure(figsize=(15, 5))
+dates = date_df["day"].values
+plt.plot(dates, xs) 
+for tr in start_dates:
+    plt.axvline(tr, color="r", linestyle="solid")
+for tr in season_changes:
+    plt.axvline(tr, color="r", linestyle="dashed")
+for pr in my_bkps:
+    if len(dates) <= pr:
+        continue
+    plt.axvline(dates[pr], color="b", linestyle="dashed")
+
+
+# %%
